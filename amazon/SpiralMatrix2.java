176ae403eec6e67e08ea0712824aeb1bb2e928d3
aaron <aaronwong668@gmail.com>
2020-01-15 01:15:34 -0800

    matrixspin

---
+++ amazon/SpiralMatrix2.java
@@ -0,0 +1,41 @@
+class SpiralMatrix2 {
+    public static void main(String[] args) {
+        int[][] ret = generateMatrix(4);
+        System.out.println(ret);
+    }
+
+    public static int[][] generateMatrix(int n) {
+        
+        int currVal = 1; // lets just assume n is positive man not stupid stuff
+        
+        int[][] matrix = new int[n][n];
+        int left = 0;
+        int top = 0;
+        int right = n - 1;
+        int bottom = n - 1;
+        if (n <= 0) {
+            return matrix;
+        }
+        while (left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = currVal++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = currVal++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = currVal++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][right] = currVal++;
+            }
+            left++;
+        }
+        return matrix;
+    }
+
+
+}
