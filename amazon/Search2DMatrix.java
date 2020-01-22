464e82fbd84fbe7a947dc30ed75f85a91b26860e
aaron <aaronwong668@gmail.com>
2020-01-22 01:01:31 -0800

    more optimal 2d Search

--- amazon/Search2DMatrix.java
+++ amazon/Search2DMatrix.java
@@ -1,15 +1,15 @@
 class Search2DMatrix {
     public static void main(String[] args) {
-        /* int[][] mat = { 
+        int[][] mat = { 
         { 1, 4, 7, 11, 15 }, 
         { 2, 5, 8, 12, 19 }, 
         { 3, 6, 9, 16, 22 }, 
         { 10, 13, 14, 17, 24 },
-        { 18, 21, 23, 26, 30 } }; */
-        int[][] mat = {
+        { 18, 21, 23, 26, 30 } };
+        /* int[][] mat = {
             { -1, 3 },
-        }; 
-        boolean found = searchMatrix(mat, 3);
+        };  */
+        boolean found = searchMatrix2(mat, 20);
         System.out.println(found);
     }
 
@@ -24,7 +24,6 @@ class Search2DMatrix {
             System.out.println(matrix[i][j]);
             if (matrix[i][j] == target) {
                 ret = true;
-                
             }
             // the first time this is true the target value will be either on top or to the
             // left of this location
@@ -33,16 +32,18 @@ class Search2DMatrix {
                 column = j;
                 row = i;
                 // check all above
-                for (int k = 0; k <= row; k++) {
+                for (int k = 0; k < row; k++) {
                     if (matrix[k][column] == target) {
                         ret = true;
-
+                        break;
                     }
                 }
-                for (int l = 0; l <= column; l++) {
-                    if (matrix[row][l] == target) {
-                        ret = true;
-
+                if (ret == false) {
+                    for (int l = 0; l < column; l++) {
+                        if (matrix[row][l] == target) {
+                            ret = true;
+                            break;
+                        }
                     }
                 }
 
@@ -62,5 +63,30 @@ class Search2DMatrix {
             }
         }
         return ret;
+    }
+
+    public static boolean searchMatrix2(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        boolean found = false;
+        while (i < matrix.length && j >= 0) {
+            System.out.println(matrix[i][j]);
+            if (target == matrix[i][j]) {
+                found = true;
+                break;
+            }
+            if (target > matrix[i][j]) {
+                i++;
+                continue;
+            } 
+            if (target < matrix[i][j]) {
+                j--;
+                continue;
+            } 
+        }
+        return found;
     }
 }
