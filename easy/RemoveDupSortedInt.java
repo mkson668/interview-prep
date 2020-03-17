ea2438917c1d214e935c679a90b60b7d3e7f5d27
aaron <aaronwong668@gmail.com>
2020-03-16 23:19:28 -0700

    use double pointer for duplicates

--- easy/RemoveDupSortedInt.java
+++ easy/RemoveDupSortedInt.java
@@ -1,24 +1,37 @@
+import java.util.*;
+
 class RemoveDupSortedInt {
     public static void main(String[] args) {
         int[] inA = new int[] {0,0,1,1,1,2,2,3,3,4};
         int ret = removeDuplicates(inA);
+        int[] inA1 = new int[] {0,1,2,2,3,0,4,2};
+        int ret1 = removeElement(inA1, 2);
     }
 
     public static int removeDuplicates(int[] nums) {
-        int currInt = 0;
-        int replaceIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                replaceIndex = i + 1;
-                currInt = nums[0];
+        int i = 0;
+        int currIntInd = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == nums[currIntInd]) {
                 continue;
-            }
-            if (nums[i] != currInt) {
-                currInt = nums[i];
-                nums[replaceIndex] = nums[i];
-                replaceIndex++;
+            } else {
+                // not equal
+                ++i;
+                nums[i] = nums[j];
+                currIntInd = j;
             }
         }
-        return replaceIndex;
+        return i + 1;
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
